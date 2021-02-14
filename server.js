90f8a3cabb034ac8f0732cf6302a4d7128026d2a
LivBurton <ojburton@yahoo.co.uk>
2021-02-14 19:06:56 +0000

    completed React client

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-// const cors = require('cors');
-const schema = require('./schema');
 const cors = require('cors');
+const schema = require('./schema');
+const path = require('path');
 
 const app = express();
 
@@ -17,7 +17,12 @@ app.use(
   })
 );
 
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-// app.use(express.static('public'));
